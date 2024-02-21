30e8f6dddfbae1b8e2656579d5fe2041eb6d09c4
saad <saadahmad01799@gmail.comm>
2024-02-21 12:24:06 +0500

    applying x functionality

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,21 +1,43 @@
 import Square from './Square';
+import { useState } from 'react';
 const Board = () => {
+  const [square, setSquare] = useState(Array(9).fill(null));
+
+  const handleSquareClick = clickedposition => {
+    setSquare(currentSquare => {
+      return currentSquare.map((squareValue, position) => {
+        if (clickedposition === position) {
+          return 'X';
+        }
+
+        return squareValue;
+      });
+    });
+  };
+  const renderSquare = position => {
+    return (
+      <Square
+        value={square[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
